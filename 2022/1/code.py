2542893d0126d37b308011dcaa5063f71d7c10c3
Chris Taylor <chris@stillbreathing.co.uk>
2022-12-03 19:32:01 +0000

    Day 1

---
+++ 2022/1/code.py
@@ -0,0 +1,19 @@
+import io
+
+file = open('2022/1/input.txt', 'r')
+lines = file.read()
+
+elves = lines.split('\n\n')
+
+calories = []
+
+for elf in elves:
+    elf_calories = elf.split('\n')
+    # print(elf_calories)
+    total_elf_calories = sum([int(i) for i in elf_calories if i.isdigit()])
+    # print(total_elf_calories)
+    calories.append(total_elf_calories)
+
+print(sorted(calories))
+
+print(max(calories))
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# advent-of-code
+# Advent of Code
+
+My code for the [Advent of Code](https://adventofcode.com/) challenge.
+
+It may be a bad idea to make this public. Don't expect genius.
